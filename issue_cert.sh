f0d40b429606bf02a6008e7cdd13d3eb197797c1
coeus-lei <42054370+coeus-lei@users.noreply.github.com>
2019-02-27 18:39:50 +0800

    Create issue_cert.sh

---
+++ issue_cert.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ ! -d "/root/rsynccert" ];then
+    mkdir /root/rsynccert
+    ~/.acme.sh/acme.sh --issue -d $1 --standalone
+else
+    DOMAIN_list=(`~/.acme.sh/acme.sh --list | awk '{print $1}' | grep -E ^$1`)
+    if [ "$1" == "$DOMAIN_list" ];then
+    	~/.acme.sh/acme.sh --issue -d $1 --standalone --force
+    else
+    	~/.acme.sh/acme.sh --issue -d $1 --standalone
+    fi
+fi
+#if [ !-d "/root/rsynccert/$1" ]
+#    ~/.acme.sh/acme.sh --issue -d $1 --standalone
+#else
+#    ~/.acme.sh/acme.sh --issue -d $1 --standalone --force
+#fi
+if [ -f "/root/mycerts/$1/$1.key" ] && [ -r "/root/mycerts/$1/$1.key" ]
+then
+	if [ ! -d "/root/rsynccert/$1" ]
+	then
+		mkdir /root/rsynccert/$1
+	fi
+	~/.acme.sh/acme.sh --installcert -d $1 --key-file /root/rsynccert/$1/$1.key --fullchain-file /root/rsynccert/$1/$1.cer
+fi
